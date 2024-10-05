82324374043bdcea715bea3c3ee56b0d58090d36
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-10-04 21:59:03 -0400

    build spectrogram

--- dataset/0_generate_spectrogram.py
+++ dataset/0_generate_spectrogram.py
@@ -33,19 +33,18 @@ def process(
     upper_freq=1.5,
 ):
     for file_dir in tqdm(recursive_files(data_dir)):
-        rel_path = os.path.relpath(file_dir, data_dir)
-        save_file = os.path.join(preprocess_dir, rel_path)
-        if os.path.exists(save_file):
-            continue
-
         try:
             csv_data = pd.read_csv(file_dir)
         except FileNotFoundError:
             print(f"skipped {file_dir}")
             continue
 
-        t = np.array(csv_data["time_rel(sec)"])
-        v = np.array(csv_data["velocity(m/s)"])
+        try:
+            t = np.array(csv_data["time_rel(sec)"])
+            v = np.array(csv_data["velocity(m/s)"])
+        except KeyError:
+            t = np.array(csv_data["rel_time(sec)"])
+            v = np.array(csv_data["velocity(c/s)"])
 
         sr = get_sampling_rate(t)
         if not np.isclose(sr, default_sr):
@@ -57,6 +56,8 @@ def process(
         mask = (spec_f > lower_freq) & (spec_f < upper_freq)
         spec_f, sxx = spec_f[mask], sxx[mask, :]
 
+        rel_path = os.path.relpath(file_dir, data_dir)
+        save_file = os.path.join(preprocess_dir, rel_path)
         save_path = os.path.dirname(save_file)
         if not os.path.exists(save_path):
             os.makedirs(save_path)
@@ -64,8 +65,11 @@ def process(
         np.savez(f"{save_file}.npz", spec_f, spec_t, sxx)
 
 
-def main(data_dir="./data/data/", preprocess_dir="./dataset/data/"):
-    for planet in ["lunar/", "mars/"]:
+def main(
+    data_dir="./data/data/",
+    preprocess_dir="./dataset/data/",
+):
+    for planet in ["mars/", "lunar/"]:
         for dataset in ["test/", "training/"]:
             process(
                 f"{data_dir}{planet}{dataset}data/",
