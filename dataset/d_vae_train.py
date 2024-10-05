3ba1e667459746602dd5942702eac8e041c79f2d
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-10-05 14:16:51 -0400

    vae dataset

--- dataset/d_vae_train.py
+++ dataset/d_vae_train.py
@@ -12,49 +12,86 @@ import c_generate_catalog as c
 
 
 class QuakeDatasetVAE(nn_data.Dataset):
-    def __init__(self, train=True, lunar=True) -> None:
+    def __init__(self, train=True, lunar=True, debug=False) -> None:
         super().__init__()
 
         self.train = train
         self.lunar = lunar
 
         self.load_catalog()
-        self.load_db()
+        self.compute_index()
 
-    def load_catalog(
-        self,
-    ):
+        if debug:
+            self.verify_indices()
+
+    def load_catalog(self):
         if self.lunar:
             catalog_dir = c.CATALOG_LUNAR
             file_dir = f"{a.PREPROCESS_DIR}lunar/training/S12_GradeA/"
-            file_ext = ".csv.npz"
         else:
             catalog_dir = c.CATALOG_MARS
             file_dir = f"{a.PREPROCESS_DIR}mars/training/"
-            file_ext = ".npz"
 
         catalog_data = pd.read_csv(catalog_dir)
 
-        for _, row in catalog_data.iterrows():
-            filename = f"{row['filename']}{file_ext}"
-            time_rel = row["time_rel(sec)"]
+        allocated = False
+        all_len = len(catalog_data)
+
+        for i, row in catalog_data.iterrows():
+            filename = f"{row['filename']}.npz"
 
             d = np.load(f"{file_dir}{filename}")
-            spec_f, spec_t, sxx = d["spec_f"], d["spec_t"], d["sxx"]
+            f, t, sxx = d["spec_f"], d["spec_t"], d["sxx"]
 
-        ...
+            if not allocated:
+                self.f_all = torch.empty((all_len, *f.shape))
+                self.t_all = torch.empty((all_len, *t.shape))
+                self.t_size = torch.empty(all_len)
+                self.sxx_all = torch.empty((all_len, *sxx.shape))
 
-    def load_db(self):
-        if self.train:
-            self.x = torch.load(f"./prediction/x_2003-2017_train.pt")
-            self.y = torch.load(f"./prediction/y_2003-2017_train.pt")
-            return
+                self.time_rel = torch.empty(all_len)
+                allocated = True
 
-        self.x = torch.load(f"./prediction/x_2018_test.pt")
-        self.y = torch.load(f"./prediction/y_2018_test.pt")
+            self.f_all[i, : f.size] = torch.from_numpy(f)
+            self.t_all[i, : t.size] = torch.from_numpy(t)
+            self.t_size[i] = t.size
+            self.sxx_all[i, : f.size, : t.size] = torch.from_numpy(sxx)
+
+            self.time_rel[i] = row["time_rel(sec)"]
+
+    def compute_index(self, pre=4, post=124):
+        dt = torch.abs(self.t_all - self.time_rel.view(-1, 1))
+
+        n = dt.shape[0]
+
+        index_center = torch.empty((n, 1))
+        for i, series in enumerate(dt):
+            t_size = int(self.t_size[i])
+            index_center[i, 0] = torch.argmin(series[:t_size])
+
+        window_size = pre + post
+        windows = torch.tile(torch.arange(window_size), (n, 1))
+        window_index = index_center - pre + windows
+
+        self.indices = window_index % self.t_size.view(-1, 1)
+
+    def verify_indices(self):
+        import matplotlib.pyplot as plt
+
+        for i, indices in enumerate(self.indices):
+            indices = indices.type(torch.int32)
+
+            t = self.t_all[i, indices]
+            f = self.f_all[i]
+            sxx = self.sxx_all[i, :, indices]
+
+            plt.pcolormesh(t, f, sxx)
+            plt.ylabel("Frequency [Hz]")
+            plt.xlabel("Time [sec]")
+            plt.show()
 
     def __len__(self):
-        return len(self.x)
+        return self.indices.size
 
     def __getitem__(self, idx):
         return (self.x[idx], self.y[idx])
@@ -113,7 +150,9 @@ def main(
     model = QuakeVAE()
     optimizer = optim.Adam(model.parameters(), lr)
 
-    train_dataloader = nn_data.DataLoader(QuakeDatasetVAE(), batch_size=batch_size)
+    train_dataloader = nn_data.DataLoader(
+        QuakeDatasetVAE(debug=True), batch_size=batch_size
+    )
 
     for epoch in range(epochs):
         model.train()
